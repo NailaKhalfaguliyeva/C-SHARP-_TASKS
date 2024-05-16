8602952b0216cbcb8a29c822a2b47a0854f5704d
Naila <khalfaguliyevanaila@gmail.com>
2024-05-16 12:52:49 +0400

    first commit

---
+++ Person.cs
@@ -0,0 +1,34 @@
+﻿//using Class;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Class
+//{
+//    public class Person
+
+//    {
+//        string name;
+//        string surname;
+//        string age;
+//        public Person(string name, string surname, string age)
+//        {
+//            this.name = name;
+//            this.surname = surname;
+//            this.age = age;
+//            Console.WriteLine($"name: {this.name},surname : {surname}, age: {age}");
+         
+
+//        }
+//        Person name1 = new Person();
+//        Person surname1 = new Person();
+//        Person age1 = new Person();
+
+//    }
+//}
+
+     
+    
+
